97a999035e9d80cbc82efae1120aeef83ff42460
vongbhan <vongbhan.soi@gmail.com>
2019-05-01 14:38:57 -0700

    endvongbhan

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
   selector: "app-home",
@@ -7,6 +8,11 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
   constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    $(document).ready(function() {
+      $("#sidebarCollapse").on("click", function() {
+        $("#sidebar").toggleClass("active");
+      });
+    });
+  }
 }
